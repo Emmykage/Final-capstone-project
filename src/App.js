33a21be7204b5efd956ccc6f45fcf18952cf22f3
Menanya <emmiemenz@gmail.com>
2022-10-21 22:33:33 +0100

    layouts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
+import Navaigation from './components/nav/Navigation';
 
 function App() {
   return (
 
     <div className="container">
+      <Navaigation />
       <Home />
     </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from '../components/footer/Footer'
 import HeaderBody from '../components/headerBody/HeaderBody'
-import Navaigation from '../components/nav/Navigation'
+// import Navaigation from '../components/nav/Navigation'
 
 export const Home = () => {
   return (
     <>
-     <Navaigation />
+
 
     <div className='sub-container'>
         <HeaderBody />
