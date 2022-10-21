91653bdcb12b85e051ca6c58947dd095a7c1a44c
Menanya <emmiemenz@gmail.com>
2022-10-21 14:57:44 +0100

    bare surface home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Footer from '../components/footer/Footer'
 import HeaderBody from '../components/headerBody/HeaderBody'
 import Navaigation from '../components/nav/Navigation'
 
 export const Home = () => {
   return (
+    <>
+
     <div className='header'>
         <Navaigation />
+        
+        <div>
         <HeaderBody />
+        <Footer/>
+        </div>
     </div>
    
+
+    </>
   )
 }
