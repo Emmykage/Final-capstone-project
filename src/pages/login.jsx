bc4087736cdc5275dd9417c942b7c02af8fcef1c
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-25 02:06:26 +0300

    Style login screen

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,28 +7,28 @@ export default function (props) {
       <form className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
-          <div className="form-group mt-3">
+          <div className="form-group ">
             <label>Email address</label>
             <input
               type="email"
-              className="form-control mt-1"
+              className="form-control "
               placeholder="Enter email"
             />
           </div>
-          <div className="form-group mt-3">
+          <div className="form-group">
             <label>Password</label>
             <input
               type="password"
-              className="form-control mt-1"
+              className="form-control"
               placeholder="Enter password"
             />
           </div>
-          <div className="d-grid gap-2 mt-3">
+          <div className="submissions">
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
+          <p className="forgot-password text-right ">
             Forgot <a href="#">password?</a>
           </p>
         </div>
