759d505f7d11c3344ccf9fe843c2da35684085ab
Menanya <emmiemenz@gmail.com>
2022-10-25 20:11:27 +0100

    init

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Reservation from '../components/Reservations/Reservation'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchReservation } from '../redux/reservationReducer'
+// const reservations = [{
+//   name: 'toyo',
+//   brand: 'toyota wheeler',
+//   color: 'nevada',
+//   date: '2022-10-24'
+// },
+// {
+//   name: 'Hyunda',
+//   brand: 'Hyundai alvero',
+//   city: 'Las vegas',
+//   date: '2022-10-24'
+// },
+// {
+//   name: 'innoson',
+//   brand: 'toyo',
+//   city: 'nevada',
+//   date: '2022-10-24'
+// },
+// {
+//   model: 'toyo',
+//   city: 'nevada',
+//   date: '2022-10-24'
+// },
+// {
+//   model: 'toyo',
+//   city: 'nevada',
+//   date: '2022-10-24'
+// }]
 
 
-const reservations = [{
-  name: 'toyo',
-  brand: 'toyota wheeler',
-  color: 'nevada',
-  date: '2022-10-24'
-},
-{
-  name: 'Hyunda',
-  brand: 'Hyundai alvero',
-  city: 'Las vegas',
-  date: '2022-10-24'
-},
-{
-  name: 'innoson',
-  brand: 'toyo',
-  city: 'nevada',
-  date: '2022-10-24'
-},
-{
-  model: 'toyo',
-  city: 'nevada',
-  date: '2022-10-24'
-},
-{
-  model: 'toyo',
-  city: 'nevada',
-  date: '2022-10-24'
-}]
 const Reservations = () => {
+  const dispatch = useDispatch();
+  const {reservations} = useSelector((state) => state.reservation)
+  useEffect(()=>{
+    dispatch(fetchReservation)
+  }, [] )
+  // if(isLoading){
+  //   return(
+  //     <div><h1> Loading...</h1></div>
+  //   )
+  // }
   return (
     <div className='sub-container'>
 
--- src/redux/reservationReducer.js
+++ src/redux/reservationReducer.js
@@ -1,40 +1,73 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    reservations: [],
-    isLoading: true
+    reservations: [{
+        name: 'toyo',
+        brand: 'toyota wheeler',
+        color: 'nevada',
+        date: '2022-10-24'
+      },
+      {
+        name: 'Hyunda',
+        brand: 'Hyundai alvero',
+        city: 'Las vegas',
+        date: '2022-10-24'
+      },
+      {
+        name: 'innoson',
+        brand: 'toyo',
+        city: 'nevada',
+        date: '2022-10-24'
+      },
+      {
+        model: 'toyo',
+        city: 'nevada',
+        date: '2022-10-24'
+      },
+      {
+        model: 'toyo',
+        city: 'nevada',
+        date: '2022-10-24'
+      }],
+    // isLoading: true
 }
 
 const reservationSlice = createSlice({
     name: 'reservation',
     initialState,
-    extraReducers: {
-        [fetchReservation.pending]: (state) => ({
-            ...state,
-            isLoading: true
-        }),
-
-        [fetchReservation.fulfilled]: (state, action) =>({
-            ...state,
-            reservations: action.payload,
-            isLoading: false
-
-        }),
-        [fetchReservation.rejected]: (state) => ({
-            ...state,
-            isLoading: false
-        }),
-        clearReservation: (state) =>{
-            state.reservations = []
-        }
+    reducers: {
+        fetchReservation: (state)=>({
+            ...state
+        })
     }
+    // extraReducers: {
+    //     [fetchReservation.pending]: (state) => ({
+    //         ...state,
+    //         isLoading: true
+    //     }),
+
+    //     [fetchReservation.fulfilled]: (state, action) =>({
+    //         ...state,
+    //         reservations: action.payload,
+    //         isLoading: false
+
+    //     }),
+    //     [fetchReservation.rejected]: (state) => ({
+    //         ...state,
+    //         isLoading: false
+    //     }),
+    //     clearReservation: (state) =>{
+    //         state.reservations = []
+    //     }
+    // }
 })
 
-const fetchReservation = createAsyncThunk('bikes/fetchReservations', async (user) =>{
-    const response = await fetch('localhost:300').then(res=> res.json);
-    // const data = await response.json()
+// const fetchReservation = createAsyncThunk('bikes/fetchReservations', async (user) =>{
+//     const response = await fetch('localhost:300').then(res=> res.json);
+//     // const data = await response.json()
 
-    return response;
-})
+//     return response;
+// })
 
+export const { fetchReservation } = reservationSlice.actions
 export default reservationSlice.reducer
