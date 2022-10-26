8f4a2b3b7330f202950664e2767661497154ac05
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-26 21:29:20 +0300

    Add motocycles reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import motocycleReducer from './motocycles/motocycles';
+
+const rootReducer = combineReducers({
+  motocycles: motocycleReducer, 
+});
+
+const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk, logger));
+
+export default store;
