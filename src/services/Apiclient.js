edb7963019ce3caef5a361783e96b889a1988ad1
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-26 21:51:37 +0300

    Login user API integration

--- src/services/Apiclient.js
+++ src/services/Apiclient.js
@@ -1,8 +1,8 @@
 export default class ApiClient {
-    static BASE_URL = 'http://127.0.0.1:3000';
+    static BASE_URL = 'http://127.0.0.1:3000/api/v1/';
   
     static fetchMotocycles = async () => {
-      const response = await fetch(this.BASE_URL, {
+      const response = await fetch(`${this.BASE_URL}/motorcycles`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
@@ -10,5 +10,15 @@ export default class ApiClient {
       });
       const responseData = await response.json();
       return responseData;
+    };
+
+    static loginUser = ({ name, password }) => {
+        const response = fetch(`${this.BASE_URL}/login?name=${name}?password=${password}`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        });
+        return response;
     };
   }
