94a712f71ec8e5c0a73086a6355e8876687aac9d
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-26 22:42:31 +0300

    Fix eslint

--- src/services/Apiclient.js
+++ src/services/Apiclient.js
@@ -2,33 +2,33 @@ export default class ApiClient {
     static BASE_URL = 'http://127.0.0.1:3000/api/v1/';
 
     static fetchMotocycles = async () => {
-      const response = await fetch(`${this.BASE_URL}/motorcycles`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-      });
-      const responseData = await response.json();
-      return responseData;
+        const response = await fetch(`${this.BASE_URL}/motorcycles`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            },
+        });
+        const responseData = await response.json();
+        return responseData;
     };
 
     static loginUser = ({ name, password }) => {
-      const response = fetch(`${this.BASE_URL}/login?name=${name}?password=${password}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
+        const response = fetch(`${this.BASE_URL}/login?name=${name}?password=${password}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response;
     };
 
     static registerUser = ({ name, password }) => {
-      const response = fetch(`${this.BASE_URL}/create?name=${name}?password=${password}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
+        const response = fetch(`${this.BASE_URL}/create?name=${name}?password=${password}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response;
     };
 }
