fa2d1dbfdf7078e445a353e6bd114ab54b7ba6ab
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-26 22:16:45 +0300

    fix form type miss match

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -5,16 +5,26 @@ import ApiClient from '../../services/ApiClient';
 const LoginScreen = (props) => {
   const [name, setName] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (name && password) {
-      const response = await  ApiClient.loginUser({name, password});
-      if (response && response.status === 200) {
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-        props.history.push("/");
+    setError(null);
+    try {
+      if (name && password) {
+        const response = await  ApiClient.loginUser({name, password});
+        if (response && response.status === 200) {
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("user", JSON.stringify(response.data.user));
+          props.history.push("/");
+        } else {
+          setError({message: "Invalid credentials"});
+        }
+      } else{
+        setError({message: "Please enter both your name and password"});
       }
+    } catch (error) {
+      setError({message: "Invalid credentials"});
     }
   };
   return (
@@ -28,12 +38,15 @@ const LoginScreen = (props) => {
              <Link to="/auth/register"> Sign up</Link>
             </span>
           </div>
+          {
+            error && <div className="alert alert-danger">{error.message}</div>
+          }
           <div className="form-group ">
-            <label>Email address</label>
+            <label>User Name</label>
             <input
-              type="email"
+              type="text"
               className="form-control "
-              placeholder="Enter email"
+              placeholder="Enter your name"
               onChange={(e) => setName(e.target.value)}
             />
           </div>
