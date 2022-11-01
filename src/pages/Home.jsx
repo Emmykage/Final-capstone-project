faef8eab0fbba042faf963a6321b6ceadff557ce
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-11-01 23:57:22 +0300

    Add authorization for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+
+
+import React, {useEffect} from 'react'
 import Footer from '../components/footer/Footer'
 import HeaderBody from '../components/headerBody/HeaderBody'
+import { useNavigate} from 'react-router-dom'
 // import Navaigation from '../components/nav/Navigation'
 
 export const Home = () => {
+  const navigation = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      navigation('/splash_page')
+    }
+  }, [])
+
   return (
-    <>
-
-
     <div className='sub-container'>
         <HeaderBody />
         <Footer/>
        
     </div>
-   
-
-    </>
   )
 }
