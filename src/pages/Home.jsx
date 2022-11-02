30908fa2126b2086b9fba90151b05470d5668471
Menanya <emmiemenz@gmail.com>
2022-11-02 17:07:27 +0100

    add log out function and adjust rounting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,7 @@ import { useNavigate} from 'react-router-dom'
 // import Navaigation from '../components/nav/Navigation'
 
 export const Home = () => {
-  const navigation = useNavigate();
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (!token) {
-      navigation('/splash_page')
-    }
-  }, [])
+ 
 
   return (
     <div className='sub-container'>
